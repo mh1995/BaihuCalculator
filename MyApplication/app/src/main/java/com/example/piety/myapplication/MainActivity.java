058f2190b91ccc0f3a2afafebe0d23cfda8a4de3
mh <813601376@qq.com>
2017-03-23 12:08:11 +0800

    bug修修复_0001

--- MyApplication/app/src/main/java/com/example/piety/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/piety/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -15,13 +16,16 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int m=4;
     double p = 0;
-    int[] TN = new int[4];
-    int[] HN = new int[4];
-    int[] HX = new int[4];
-    int[] rsTN = new int[4];
-    int[] rsHN = new int[4];
+    int[] TN = new int[4];//存储玩家拖鸟数目
+    int[] HN = new int[4];//存储玩家活鸟数目
+    int[] HX = new int[4];//存储玩家胡子数目
+    int[] resultTN = new int[4];//存储玩家拖鸟输赢结果
+    int[] resultHN = new int[4];//存储玩家活鸟输赢结果
+    EditText[] edTN = new EditText[4];
+    EditText[] edHN = new EditText[4];
+    EditText[] edHX = new EditText[4];
     private Button btnExit,btnCalaulate,btnReset;
-    private EditText price,tn1,tn2,tn3,tn4,hn1,hn2,hn3,hn4,hx1,hx2,hx3,hx4;
+    private EditText price;
     private TextView rs1,rs2,rs3,rs4,role4;
     private RadioButton three;
     @Override
@@ -30,23 +34,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnCalaulate =(Button) findViewById(R.id.btn_result);
         price = (EditText)findViewById(R.id.et_price);
-        tn1= (EditText)findViewById(R.id.et_tn1);
-        tn2= (EditText)findViewById(R.id.et_tn2);
-        tn3= (EditText)findViewById(R.id.et_tn3);
-        tn4= (EditText)findViewById(R.id.et_tn4);
-        hn1= (EditText)findViewById(R.id.et_hn1);
-        hn2= (EditText)findViewById(R.id.et_hn2);
-        hn3= (EditText)findViewById(R.id.et_hn3);
-        hn4= (EditText)findViewById(R.id.et_hn4);
-        hx1= (EditText)findViewById(R.id.et_hx1);
-        hx2= (EditText)findViewById(R.id.et_hx2);
-        hx3= (EditText)findViewById(R.id.et_hx3);
-        hx4= (EditText)findViewById(R.id.et_hx4);
+        edTN[0]= (EditText)findViewById(R.id.et_tn1);
+        edTN[1]= (EditText)findViewById(R.id.et_tn2);
+        edTN[2]= (EditText)findViewById(R.id.et_tn3);
+        edTN[3]= (EditText)findViewById(R.id.et_tn4);
+        edHN[0]= (EditText)findViewById(R.id.et_hn1);
+        edHN[1]= (EditText)findViewById(R.id.et_hn2);
+        edHN[2]= (EditText)findViewById(R.id.et_hn3);
+        edHN[3]= (EditText)findViewById(R.id.et_hn4);
+        edHX[0]= (EditText)findViewById(R.id.et_hx1);
+        edHX[1]= (EditText)findViewById(R.id.et_hx2);
+        edHX[2]= (EditText)findViewById(R.id.et_hx3);
+        edHX[3]= (EditText)findViewById(R.id.et_hx4);
         rs1 = (TextView) findViewById(R.id.tv_rs1);
         rs2 = (TextView) findViewById(R.id.tv_rs2);
         rs3 = (TextView) findViewById(R.id.tv_rs3);
         rs4 = (TextView) findViewById(R.id.tv_rs4);
         role4 = (TextView)findViewById(R.id.tv_07);
+        setNumber();
         three = (RadioButton) findViewById(R.id.rb_three);
         //选择人数
         three.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -56,17 +61,17 @@ public class MainActivity extends AppCompatActivity {
                 {
                     m=3;
                     role4.setEnabled(false);
-                    hn4.setEnabled(false);
-                    tn4.setEnabled(false);
-                    hx4.setEnabled(false);
+                    edHN[3].setEnabled(false);
+                    edTN[3].setEnabled(false);
+                    edHX[3].setEnabled(false);
                     rs4.setEnabled(false);
                 }
                 else{
                     m=4;
                     role4.setEnabled(true);
-                    hn4.setEnabled(true);
-                    tn4.setEnabled(true);
-                    hx4.setEnabled(true);
+                    edHN[3].setEnabled(true);
+                    edTN[3].setEnabled(true);
+                    edHX[3].setEnabled(true);
                     rs4.setEnabled(true);
                 }
             }
@@ -77,37 +82,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 p = Double.parseDouble(price.getText().toString().equals("")?"0":price.getText().toString());
-                String t = null;
-                t=tn1.getText().toString();
-                TN[0]=Integer.parseInt(t.equals("")?"0":t);
-                t=tn2.getText().toString();
-                TN[1]=Integer.parseInt(t.equals("")?"0":t);
-                t=tn3.getText().toString();
-                TN[2]=Integer.parseInt(t.equals("")?"0":t);
-                t=tn4.getText().toString();
-                TN[3]=Integer.parseInt(t.equals("")?"0":t);
-                t=hn1.getText().toString();
-                HN[0]=Integer.parseInt(t.equals("")?"0":t);
-                t=hn2.getText().toString();
-                HN[1]=Integer.parseInt(t.equals("")?"0":t);
-                t=hn3.getText().toString();
-                HN[2]=Integer.parseInt(t.equals("")?"0":t);
-                t=hn4.getText().toString();
-                HN[3]=Integer.parseInt(t.equals("")?"0":t);
-                t=hx1.getText().toString();
-                HX[0]=(int)Math.rint(Integer.parseInt(t.equals("")?"0":t)/10.0)*10;
-                t=hx2.getText().toString();
-                HX[1]=(int)Math.rint(Integer.parseInt(t.equals("")?"0":t)/10.0)*10;
-                t=hx3.getText().toString();
-                HX[2]=(int)Math.rint(Integer.parseInt(t.equals("")?"0":t)/10.0)*10;
-                t=hx4.getText().toString();
-                HX[3]=(int)Math.rint(Integer.parseInt(t.equals("")?"0":t)/10.0)*10;
+                for(int i=0;i<m;i++){
+                    TN[i]=Integer.parseInt(edTN[i].getText().toString().equals("")?"0":edTN[i].getText().toString());
+                    HN[i]=Integer.parseInt(edHN[i].getText().toString().equals("")?"0":edHN[i].getText().toString());
+                    HX[i]=Integer.parseInt(edHX[i].getText().toString().equals("")?"0":edHX[i].getText().toString());
+                }
                 cTuoNiao();
+                hxRint();
                 cHuoNiao();
-                rs1.setText(rsHN[0]+rsTN[0]+"");
-                rs2.setText(rsHN[1]+rsTN[1]+"");
-                rs3.setText(rsHN[2]+rsTN[2]+"");
-                rs4.setText(rsHN[3]+rsTN[3]+"");
+                rs1.setText(resultHN[0]+resultTN[0]+"");
+                rs2.setText(resultHN[1]+resultTN[1]+"");
+                rs3.setText(resultHN[2]+resultTN[2]+"");
+                rs4.setText(resultHN[3]+resultTN[3]+"");
             }
         });
         //退出
@@ -124,18 +110,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 price.setText("0");
-                hn1.setText("0");
-                hn2.setText("0");
-                hn3.setText("0");
-                hn4.setText("0");
-                tn1.setText("0");
-                tn2.setText("0");
-                tn3.setText("0");
-                tn4.setText("0");
-                hx1.setText("0");
-                hx2.setText("0");
-                hx3.setText("0");
-                hx4.setText("0");
+                for(int i=0;i<m;i++){
+                    edHN[i].setText("0");
+                    edTN[i].setText("0");
+                    edHX[i].setText("0");
+                }
                 rs1.setText("0");
                 rs2.setText("0");
                 rs3.setText("0");
@@ -144,19 +123,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    //默认打开数字键盘
+    public void setNumber(){
+        price.setInputType(EditorInfo.TYPE_CLASS_NUMBER);
+        for(int i=0;i<4;i++) {
+            edHN[i].setInputType(EditorInfo.TYPE_CLASS_NUMBER);
+            edTN[i].setInputType(EditorInfo.TYPE_CLASS_NUMBER);
+            edHX[i].setInputType(EditorInfo.TYPE_CLASS_NUMBER);
+        }
+    }
+    //计算拖鸟输赢
     public void cTuoNiao(){
         for(int i = 0;i<m;i++) {
-            rsTN[i]=0;
+            resultTN[i]=0;
             for(int j = 0;j<m;j++){
-                rsTN[i]+=(TN[i]-TN[j]);
+               if(HX[i]!=HX[j]){
+                   if(HX[i]>HX[j])resultTN[i]+=(TN[i]+TN[j]);
+                   else resultTN[i]-=(TN[i]+TN[j]);
+               }
             }
         }
     }
+    //胡子四舍五入
+    public void hxRint()
+    {
+        for(int i=0;i<4;i++)
+        {
+            if(Math.abs(HX[i])==5) HX[i]++;
+            HX[i]=(int) Math.rint(HX[i]/10.0)*10;
+        }
+    }
+    //计算活鸟输赢
     public void cHuoNiao(){
         for(int i = 0;i<m;i++) {
-            rsHN[i]=0;
+            resultHN[i]=0;
             for(int j = 0;j<m;j++){
-                rsTN[i]+=((HX[i]-HX[j])*p*(HN[i]+1)*(HN[j]+1));
+                resultHN[i]+=((HX[i]-HX[j])*p*(HN[i]+1)*(HN[j]+1));
             }
         }
     }
